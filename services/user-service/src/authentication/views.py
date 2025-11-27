89889ffd231f02576e885fc1598076988035c44e
fatemehamini.0091 <fatemehamini.0091@gmail.com>
2025-11-27 17:24:42 +0330

    views of user

--- services/user-service/src/authentication/views.py
+++ services/user-service/src/authentication/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from logging import raiseExceptions
 
-# Create your views here.
+from django.core.serializers import serialize
+from django.utils.autoreload import raise_last_exception
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+
+from .serializers import RegisterSerializer, LoginSerializer
+
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        "refresh": str(refresh),
+        "access" : str(refresh.access_token)
+    }
+
+class RegisterView(generics.CreateAPIView):
+    serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        user = serializer.validated_data["user"]
+        tokens = get_tokens_for_user(user)
+
+        return Response(tokens, status=status.HTTP_200_OK)
+
